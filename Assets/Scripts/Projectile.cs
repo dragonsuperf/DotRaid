b0ec241222a53e1fb718bce1db4cbd772c00f3d3
dragonsuperf <dragonsuperf@gmail.com>
2019-05-06 01:03:43 +0900

    projectile 클래스 수정, 보스 패턴 추가
    projectile 클래스 수정, 보스 패턴 추가

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    protected float speed = 1.0f;
+    protected float liftTime = 0.0f;
+    protected Vector2 targetVec;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,14 +15,39 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
 
-    public void Set(float lifeTime, Vector2 pos)
+    public void Set(float lifeTime, float speed, Vector2 pos, Vector2 target)
     {
+        this.speed = speed;
         transform.position = pos;
         gameObject.SetActive(true);
         StartCoroutine(WaitForDisable(lifeTime));
+
+        Vector3 vectorToTarget = target - (Vector2)transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+
+
+        GetComponent<Rigidbody2D>().AddForce((target - (Vector2)transform.position).normalized * speed);
+    }
+
+    public void Set(float lifeTime, float speed, Vector2 pos, Vector2 target, float extraAngle)
+    {
+        this.speed = speed;
+        transform.position = pos;
+        gameObject.SetActive(true);
+        StartCoroutine(WaitForDisable(lifeTime));
+        
+        Vector3 vectorToTarget = target - (Vector2)transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90 + extraAngle;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+
+        Vector2 normal = Quaternion.Euler(0, 0, extraAngle) * (target - (Vector2)transform.position).normalized;
+
+        GetComponent<Rigidbody2D>().AddForce(normal * speed);
     }
 
     IEnumerator WaitForDisable(float lifeTime)
