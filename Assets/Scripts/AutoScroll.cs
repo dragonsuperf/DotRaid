46d7f1810bae8d5f3fc11e80ebe12fea6aeea389
dragonsuperf <dragonsuperf@gmail.com>
2019-06-15 15:51:10 +0900

    Menu, Intro 추가

---
+++ Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AutoScroll : MonoBehaviour
+{
+    [SerializeField]
+    private float scrollSpeed = 0.4f;
+    private float maxScroll;
+    private ScrollRect scrollRect;
+    private RectTransform contenRectTransform;
+    private Vector2 defaultPosition;
+    private bool canMove;
+    private void Start()
+    {
+        this.scrollRect = GetComponent<ScrollRect>();
+        this.contenRectTransform = this.scrollRect.content;
+        this.maxScroll = this.contenRectTransform.rect.yMax;
+        canMove = true;
+        //this.StartCoroutine(this.ActivateAutoMove());
+    }
+    private void Update()
+    {
+        bool hasScrolled = this.contenRectTransform.position.y > this.maxScroll;
+        if (canMove & !hasScrolled)
+            this.Move();
+        else
+            SceneManager.LoadScene("SampleScene");
+    }
+    private void Move()
+    {
+        Vector2 contentPosition = this.contenRectTransform.position;
+        Vector2 newPosition = new Vector2(contentPosition.x, contentPosition.y + this.scrollSpeed);
+        this.contenRectTransform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/CutSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CutSceneManager : MonoBehaviour
+{
+    List<GameObject> imgList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Need To Call Images
+        for (int i = 1; i <= 4; i++)
+        {
+            imgList.Add(Instantiate(Resources.Load("UIs/Stage1/Scene_" + i) as GameObject, transform));
+        }
+    }
+}
